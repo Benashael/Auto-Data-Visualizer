5944919933bc748321d5be2b8794720e5dbe79e1
Ben Ashael <114052702+Benashael@users.noreply.github.com>
2023-10-12 11:39:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
-import zipfile
-import io
 
 # Set Streamlit page title and icon
 st.set_page_config(page_title="Data Viz App", page_icon=":bar_chart:")
@@ -18,7 +16,7 @@ page = st.sidebar.selectbox("Select a Page", ["Data Visualizer", "Tutorial"])
 if page == "Data Visualizer":
     # Option to upload a CSV file or use an example dataset
     st.sidebar.title("Choose Data Source")
-    data_source = st.sidebar.radio("Select Data Source", ("Upload CSV", "Use Example Dataset"))
+    data_source = st.sidebar.radio("Select Data Source", ["Upload CSV", "Use Example Dataset"])
 
     data = None  # Initialize data as None
 
@@ -36,20 +34,19 @@ if page == "Data Visualizer":
         st.sidebar.subheader("Example Datasets")
         example_datasets = {
             "Iris Dataset": "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv",
-            "Titanic Dataset": "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv",
         }
         selected_example = st.sidebar.selectbox("Select an example dataset", list(example_datasets.keys()))
 
         try:
-            data = pd.read_csv(example_datasets[selected_example])
+            data = pd.read_csv(example_datasets.get(selected_example, ""))
         except Exception as e:
             st.error(f"Error: {str(e)}")
             st.stop()  # Stop execution if there's an error
 
-    if data is not None:  # Check if data is loaded successfully
-        # Sidebar - Variable selection and interaction
+    if data is not None and not data.empty:  # Check if data is loaded successfully
+        # Automatically select visualization options
         st.sidebar.subheader("Visualization Settings")
-        selected_columns = st.sidebar.multiselect("Select columns for visualization", data.columns)
+        selected_columns = data.columns
 
         # Color customization
         color = st.sidebar.color_picker("Select a color", "#ff5733")
@@ -57,118 +54,61 @@ if page == "Data Visualizer":
         # Create a dictionary to store generated plots
         generated_plots = {}
 
-        # Add interaction for Matplotlib Line Plot
-        matplotlib_line_plot = st.sidebar.checkbox("Line Plot (Matplotlib)")
-        if matplotlib_line_plot:
-            st.sidebar.write("### Matplotlib Line Plot Options")
-            x_column = st.sidebar.selectbox("X-axis", selected_columns)
-            y_column = st.sidebar.selectbox("Y-axis", selected_columns)
-            line_label = st.sidebar.text_input("Line Label", "Line")
-            line_style = st.sidebar.selectbox("Line Style", ["-", "--", "-.", ":", "None"])
-            
-            if st.button("Plot Matplotlib Line Plot"):
-                st.write("### Matplotlib Line Plot")
-                fig, ax = plt.subplots()
-                ax.plot(data[x_column], data[y_column], label=line_label, color=color, linestyle=line_style)
-                ax.set_xlabel(x_column)
-                ax.set_ylabel(y_column)
-                ax.legend()
-                generated_plots["Matplotlib Line Plot"] = fig
+        # Matplotlib Line Plot (Automated)
+        if len(selected_columns) > 1:
+            x_column = selected_columns[0]  # Automatically select the first column
+            y_column = selected_columns[1]  # Automatically select the second column
+            line_label = "Line"  # Use a default line label
+            line_style = "-"  # Use a default line style
+            fig, ax = plt.subplots()
+            ax.plot(data[x_column], data[y_column], label=line_label, color=color, linestyle=line_style)
+            ax.set_xlabel(x_column)
+            ax.set_ylabel(y_column)
+            ax.legend()
+            generated_plots["Matplotlib Line Plot"] = fig
 
-        # Add interaction for Seaborn Pairplot
-        seaborn_pairplot = st.sidebar.checkbox("Pairplot (Seaborn)")
-        if seaborn_pairplot:
-            if st.button("Plot Seaborn Pairplot"):
-                st.write("### Seaborn Pairplot")
-                pairplot = sns.pairplot(data[selected_columns], palette=color)
-                generated_plots["Seaborn Pairplot"] = pairplot
+        # Seaborn Pairplot (Automated)
+        if len(selected_columns) > 1:
+            pairplot = sns.pairplot(data[selected_columns], palette=color)
+            generated_plots["Seaborn Pairplot"] = pairplot
 
-        # Add interaction for Plotly Scatter Plot
-        plotly_scatter_plot = st.sidebar.checkbox("Scatter Plot (Plotly)")
-        if plotly_scatter_plot:
-            st.sidebar.write("### Plotly Scatter Plot Options")
-            x_column = st.sidebar.selectbox("X-axis", selected_columns)
-            y_column = st.sidebar.selectbox("Y-axis", selected_columns)
-            color_column = st.sidebar.selectbox("Color by", selected_columns)
-            
-            if st.button("Plot Plotly Scatter Plot"):
-                st.write("### Plotly Scatter Plot")
-                scatter_fig = px.scatter(data, x=data[x_column], y=data[y_column], color=data[color_column])
-                generated_plots["Plotly Scatter Plot"] = scatter_fig
+        # Plotly Scatter Plot (Automated)
+        if len(selected_columns) > 2:
+            x_column = selected_columns[0]  # Automatically select the first column
+            y_column = selected_columns[1]  # Automatically select the second column
+            color_column = selected_columns[2]  # Automatically select the third column
+            scatter_fig = px.scatter(data, x=data[x_column], y=data[y_column], color=data[color_column])
+            generated_plots["Plotly Scatter Plot"] = scatter_fig
 
-        # Add interaction for Seaborn Violin Plot
-        seaborn_violin_plot = st.sidebar.checkbox("Violin Plot (Seaborn)")
-        if seaborn_violin_plot:
-            if st.button("Plot Seaborn Violin Plot"):
-                st.write("### Seaborn Violin Plot")
-                violin_plot = sns.violinplot(data=data[selected_columns], inner="quart", palette=color)
-                generated_plots["Seaborn Violin Plot"] = violin_plot
+        # Seaborn Violin Plot (Automated)
+        if len(selected_columns) > 1:
+            violin_plot = sns.violinplot(data=data[selected_columns], inner="quart", palette=color)
+            generated_plots["Seaborn Violin Plot"] = violin_plot
 
-        # Add interaction for Matplotlib Area Plot
-        matplotlib_area_plot = st.sidebar.checkbox("Area Plot (Matplotlib)")
-        if matplotlib_area_plot:
-            st.sidebar.write("### Matplotlib Area Plot Options")
-            area_column = st.sidebar.selectbox("Select column for the Area Plot", selected_columns)
-            if st.button("Plot Matplotlib Area Plot"):
-                st.write("### Matplotlib Area Plot")
-                fig, ax = plt.subplots()
-                area_plot = data[selected_columns].plot(kind="area", ax=ax, color=color)
-                ax.set_xlabel("X-axis")
-                ax.set_ylabel("Y-axis")
-                ax.legend()
-                generated_plots["Matplotlib Area Plot"] = fig
+        # Seaborn Heatmap (Automated)
+        if len(selected_columns) > 1:
+            heatmap = sns.heatmap(data[selected_columns].corr(), annot=True, cmap="coolwarm")
+            generated_plots["Seaborn Heatmap"] = heatmap
 
-        # Add interaction for Seaborn Heatmap
-        seaborn_heatmap = st.sidebar.checkbox("Heatmap (Seaborn)")
-        if seaborn_heatmap:
-            if st.button("Plot Seaborn Heatmap"):
-                st.write("### Seaborn Heatmap")
-                heatmap = sns.heatmap(data[selected_columns].corr(), annot=True, cmap="coolwarm")
-                generated_plots["Seaborn Heatmap"] = heatmap
-
-        # Add interaction for Matplotlib Pie Chart
-        matplotlib_pie_chart = st.sidebar.checkbox("Pie Chart (Matplotlib)")
-        if matplotlib_pie_chart:
-            if st.button("Plot Matplotlib Pie Chart"):
-                st.write("### Matplotlib Pie Chart")
-                fig, ax = plt.subplots()
-                pie_chart = ax.pie(data[selected_columns].sum(), labels=selected_columns, autopct='%1.1f%%', startangle=140)
-                generated_plots["Matplotlib Pie Chart"] = fig
+        # Matplotlib Pie Chart (Automated)
+        if len(selected_columns) > 1:
+            fig, ax = plt.subplots()
+            pie_chart = ax.pie(data[selected_columns].sum(), labels=selected_columns, autopct='%1.1f%%', startangle=140)
+            generated_plots["Matplotlib Pie Chart"] = fig
 
         # Display the generated plots
-        if generated_plots:
-            st.subheader("Generated Plots")
-            for plot_name, plot in generated_plots.items():
-                if isinstance(plot, plt.Figure):
-                    st.write(f"**{plot_name}**")
-                    st.pyplot(plot)
-                else:
-                    st.write(f"**{plot_name}**")
-                    st.pyplot(plot)
+        st.subheader("Generated Plots")
+        for plot_name, plot in generated_plots.items():
+            st.write(f"**{plot_name}**")
 
-        # Option to save generated plots
-        if st.button("Save Generated Plots"):
-            zip_buffer = io.BytesIO()
-            with st.spinner("Saving..."):
-                with zipfile.ZipFile(zip_buffer, 'w') as zipf:
-                    for plot_name, plot in generated_plots.items():
-                        if isinstance(plot, plt.Figure):
-                            plotfile = io.BytesIO()
-                            plot.savefig(plotfile, format="png")
-                            plotfile.seek(0)
-                            zipf.writestr(f"{plot_name}.png", plotfile.read())
-                        else:
-                            plot_bytes = plot.to_image(format="png")
-                            zipf.writestr(f"{plot_name}.png", plot_bytes)
+            if isinstance(plot, plt.Figure):
+                st.pyplot(plot)
+            else:
+                st.plotly_chart(plot)
 
-            zip_buffer.seek(0)
-            st.write("Download Zip File")
-
-    # Add error handling if data is not loaded
     else:
         st.warning("Please provide a valid CSV file or select a different dataset.")
 
-# Tutorial page
 if page == "Tutorial":
     st.title("Tutorial - Data Visualization Metrics")
 
@@ -211,11 +151,6 @@ if page == "Tutorial":
     st.write("A pie chart is a circular chart that displays data as slices of a pie.")
     st.write("It's used to show the proportion of each category in a dataset.")
 
-    # Add more explanations for other visualization metrics
-
-    # Option to download the tutorial as a PDF
-    st.subheader("Download Tutorial as PDF")
-    tutorial_pdf = st.button("Download Tutorial PDF")
-
+   
    
 
